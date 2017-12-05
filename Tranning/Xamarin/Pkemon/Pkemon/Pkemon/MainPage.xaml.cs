ff8656bfbd147150bc5ce45e1506492c102d7260
hoanghiep97 <selllplus@gmail.com>
2017-12-05 10:20:52 +0700

    Update training
    Update training xamarin project

---
+++ Tranning/Xamarin/Pkemon/Pkemon/Pkemon/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using PokedexNET;
+
+namespace Pkemon
+{
+	public partial class MainPage : ContentPage
+	{
+        private Pokedex _pokedex = new Pokedex();
+        private Pokemon pokemon= new Pokemon();
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private void Button_clicked(object sender, EventArgs e)
+        {
+            var text = txtNumber.Text;
+            int number = int.Parse(text);
+
+                pokemon = _pokedex.GetPokemon(number);
+
+                lblName.Text = pokemon.Name;
+                imgImage.Source = ImageSource.FromUri(new Uri(pokemon.ImageUrl));
+           
+        
+        }
+
+        private void Button2_clicked(object sender, EventArgs e)
+        {
+            if(pokemon != null)
+            {
+                Navigation.PushAsync(new Page1(pokemon));
+            }
+        }
+    }
+}
---
+++ Tranning/Xamarin/Pkemon/Pkemon/Pkemon/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PokedexNET;
+
+namespace Pkemon
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 (Pokemon pokemon)
+		{
+			InitializeComponent ();
+            var pokedex = new Pokedex();
+            var info = pokedex.GetPokemonInfo(pokemon);
+
+            name.Text = info.Names.En;
+            we.Text = info.WeightEu;
+            he.Text = info.HeightEu;
+            des.Text = info.PokedexEntries.OmegaRuby.En;
+            image.Source = ImageSource.FromUri(new Uri(pokemon.ImageUrl));
+
+		}
+	}
+}
